0ef3448c5d09328711edb1af1e091ababc114149
default <ykim@fa05956kyoung-tae.local>
2019-02-02 21:26:50 -0500

    4.2 complete UIView Animations and Closures

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -10,11 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //PROPERTIES
+    
+    @IBOutlet weak var imageToPunch: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
+    //FUNCTIONS
+    
+    func animateImage(){
+        let bounds = self.imageToPunch.bounds
+        let shrinkValue: CGFloat = 60
+        //shrink our imageToPunch by 60 pixels
+        self.imageToPunch.bounds = CGRect(
+            x: self.imageToPunch.bounds.origin.x + shrinkValue,
+            y: self.imageToPunch.bounds.origin.y + shrinkValue,
+            width: self.imageToPunch.bounds.size.width - shrinkValue,
+            height: self.imageToPunch.bounds.size.height - shrinkValue)
+    
+        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: {self.imageToPunch.bounds = bounds}, completion: nil)
+    }
+    
+    
+    //ACTIONS
 
-
+    @IBAction func libraryPressed(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func cameraPressed(_ sender: UIButton) {
+        
+    }
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+       animateImage()
+    }
+    
+    
 }
 
